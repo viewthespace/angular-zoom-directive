edb84184d3446e2bf73a9c7bd5455bd7fe609c3f
Shawn O'Mara <shaw3257@gmail.com>
2015-01-05 18:02:53 -0500

    makes initial and window settings less brittle

--- src/angular-zoom-directive.js
+++ src/angular-zoom-directive.js
@@ -1,21 +1,21 @@
 app = angular.module('open-vts', [])
 
 app.directive('ovtsZoomControls', function( $window, $document, $timeout ){
-  
+
   return {
-    
+
     restrict: 'A',
-    
+
     replace: true,
-    
+
     transclude: true,
-    
+
     template: '<div class="ovts-zoom-controls"></div>',
 
     scope: {},
 
     controllerAs: 'zoom',
-    
+
     controller: function($scope){
 
       this.in = function() {
@@ -31,7 +31,7 @@ app.directive('ovtsZoomControls', function( $window, $document, $timeout ){
       }
 
       this.isMaxedIn = function() {
-        return $scope.currentStep == $scope.stepCnt;
+        return $scope.currentStep == $scope.steps.length - 1;
       }
 
       this.isMaxedOut = function() {
@@ -41,15 +41,15 @@ app.directive('ovtsZoomControls', function( $window, $document, $timeout ){
     },
 
     link: function($scope, ele, attrs, controller, transclude){
-      
+
       $timeout(function() {
-        
+
         var options = $scope.$eval(attrs.ovtsZoomControls) || {};
-      
+
         var eleControls = ele[0];
         var eleTarget = $document[0].querySelector(options.target);
 
-        var steps = [];
+        var steps = $scope.steps = [];
         var stepCnt = $scope.stepCnt = options.stepCnt || 4;
         var animation = options.animationFn || '.7s ease-out'
         var transformOrigin = options.transformOrigin || 'center top'
@@ -62,18 +62,36 @@ app.directive('ovtsZoomControls', function( $window, $document, $timeout ){
         var offsetY = options.offsetY || 0
         var offsetX = options.offsetX || 0
 
-        if (min == 'window') {
-            minWidth = $window.innerWidth + offsetX;
-            minHeight = $window.innerHeight + offsetY;
+        if(minWidth === 'initial') {
+          minWidth = eleTarget.clientWidth
         }
 
-        if (max == 'initial') {
-            maxWidth = Math.max(eleTarget.clientWidth, minWidth);
-            maxHeight = Math.max(eleTarget.clientHeight, minHeight);
+        if(minHeight === 'initial') {
+          minHeight = eleTarget.clientHeight
         }
 
-        if( !minHeight || !minWidth || !maxHeight || !maxWidth ){
-          throw minErr('Test')
+        if(minWidth === 'window') {
+          minWidth = $window.innerWidth
+        }
+
+        if(minHeight === 'window') {
+          minHeight = $window.innerHeight
+        }
+
+        if(maxWidth === 'initial') {
+          maxWidth = eleTarget.clientWidth
+        }
+
+        if(maxHeight === 'initial') {
+          maxHeight = eleTarget.clientHeight
+        }
+
+        if(maxWidth === 'window') {
+          maxWidth = $window.innerWidth
+        }
+
+        if(maxHeight === 'window') {
+          maxHeight = $window.innerHeight
         }
 
         transclude($scope, function(nodes){
@@ -85,52 +103,60 @@ app.directive('ovtsZoomControls', function( $window, $document, $timeout ){
         applyTransformOrigin(eleTarget, transformOrigin)
 
         $scope.$watch('currentStep', function(currentStep, oldStep){
-            if(currentStep !== oldStep){
-              applyAnimation(eleTarget, animation);
-            }
-            applyTransform(eleTarget, steps[currentStep]);
+          if(currentStep !== oldStep){
+            applyAnimation(eleTarget, animation);
+          }
+          applyTransform(eleTarget, steps[currentStep]);
         });
 
         function calculateSteps(){
           var width = eleTarget.clientWidth;
           var height = eleTarget.clientHeight;
-          var minWidthScale =  minWidth / width;
-          var minHeightScale =  minHeight / height;
-          var maxWidthScale =  maxWidth / width;
-          var maxHeightScale =  maxHeight / height;
-          var minScale = (max && min) ? Math.min(minWidthScale, minHeightScale) : Math.max(minWidthScale, minHeightScale);
-          var maxScale = (max && min) ? Math.max(maxWidthScale, maxHeightScale) : Math.min(maxWidthScale, maxHeightScale);
+          var minWidthScale =  minWidth / width || -Infinity;
+          var minHeightScale =  minHeight / height || -Infinity;
+          var maxWidthScale =  maxWidth / width || Infinity;
+          var maxHeightScale =  maxHeight / height || Infinity;
+          var minScale = Math.max(minWidthScale, minHeightScale);
+          var maxScale = Math.min(maxWidthScale, maxHeightScale);
           var minLog = Math.log(minScale);
           var maxLog = Math.log(maxScale);
-          
-          steps = [];
-          var initalStep = Math.round(x(0));
-          for (var i = 0; i <= stepCnt; i++) {
-            var step;
-            if (i < initalStep) {
-              step = leftY(i);
-            } 
-            else if(i > initalStep) {
-              step = rightY(i);
+
+
+          if(minScale > 1 || maxScale < 1) {
+
+            steps.push(1)
+
+          }else{
+
+            var initalStep = Math.round(x(0));
+
+            for (var i = 0; i <= stepCnt; i++) {
+              var step;
+              if (i < initalStep) {
+                step = leftY(i);
+              }
+              else if(i > initalStep) {
+                step = rightY(i);
+              }
+              else {
+                step = 0;
+              }
+              steps.push(Math.pow(Math.E, step));
             }
-            else {
-              step = 0;
+
+            function x(y) {
+              return stepCnt * (y - minLog) / (maxLog - minLog)
             }
-            steps.push(Math.pow(Math.E, step));      
-          }
 
-          function x(y) {
-            return stepCnt * (y - minLog) / (maxLog - minLog)
-          }
+            function leftY(x) {
+              return -minLog / initalStep * x + minLog
+            }
 
-          function leftY(x) {
-            return -minLog / initalStep * x + minLog
-          }
+            function rightY(x) {
+              return maxLog * ( x - initalStep ) / (stepCnt - initalStep);
+            }
 
-          function rightY(x) {
-            return maxLog * ( x - initalStep ) / (stepCnt - initalStep);
           }
-
           return steps.indexOf(1);
         };
 
